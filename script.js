784e55ed323cab0c974a2d4c740b33a71df124a4
AMBBDev1118 <andreimiguelbervano1118@gmail.com>
2023-05-29 15:31:00 +0800

    first commit
    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+console.log("Hello world!");
+
+const myName = "Andrei Bervano";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.display = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+var magnets = document.querySelectorAll(".magnetic");
+var strength = 50;
+
+magnets.forEach((magnet) => {
+  magnet.addEventListener("mousemove", moveMagnet);
+  magnet.addEventListener("mouseout", function (event) {
+    TweenMax.to(event.currentTarget, 1, { x: 0, y: 0, ease: Power4.easeOut });
+  });
+});
+
+function moveMagnet(event) {
+  var magnetButton = event.currentTarget;
+  var bounding = magnetButton.getBoundingClientRect();
+
+  //console.log(magnetButton, bounding)
+
+  TweenMax.to(magnetButton, 1, {
+    x:
+      ((event.clientX - bounding.left) / magnetButton.offsetWidth - 0.5) *
+      strength,
+    y:
+      ((event.clientY - bounding.top) / magnetButton.offsetHeight - 0.5) *
+      strength,
+    ease: Power4.easeOut,
+  });
+
+  //magnetButton.style.transform = 'translate(' + (((( event.clientX - bounding.left)/(magnetButton.offsetWidth))) - 0.5) * strength + 'px,'+ (((( event.clientY - bounding.top)/(magnetButton.offsetHeight))) - 0.5) * strength + 'px)';
+}
